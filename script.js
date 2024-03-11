7f9beab177972c3469c0123bca5a3346ab0c432a
AR-Lego <74335940+AR-Lego@users.noreply.github.com>
2024-03-11 17:00:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Get the width of the webpage
+var pageWidth = document.documentElement.clientWidth;
+
+// Get the height of the webpage
+var pageHeight = document.documentElement.clientHeight;
+
 function setup() {
-  createCanvas(width, height);
+  createCanvas(pageWidth, pageHeight);
 }
 
 function draw() {
