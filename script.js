5108842c00a51a2b951fdd3d9f8f785b9ff82801
AR-Lego <74335940+AR-Lego@users.noreply.github.com>
2024-03-11 17:06:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 // Get the width of the webpage
 var pageWidth = document.documentElement.clientWidth;
-
 // Get the height of the webpage
 var pageHeight = document.documentElement.clientHeight;
 
+var oOOo;
+
+class Orb {
+  Constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.velX = 0;
+    this.velY = 0;
+    this.size = 5;
+    this.color = "red";
+  }
+
+  show() {
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+  }
+
+  update() {
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
+
+
+
+
 function setup() {
   createCanvas(pageWidth, pageHeight);
+  oOOo = new Orb();
 }
 
 function draw() {
   background(220);
-  fill("red");
-  rect(20,20,20,20);
+  oOOo.show();
+  oOOo.velX = 1;
+  oOOo.velY = 2;
+  oOOo.update();
 }
